b9e9dfcc425d331bccf2ae6c6a6d4fde46949b50
Bradley Bossard <bradleybossard@gmail.com>
2021-05-27 15:15:12 -0700

    adding Rectangle component

---
+++ components/Rectangle.tsx
@@ -0,0 +1,23 @@
+// src/component/Rectangle/Rectangle.tsx
+import React, { useEffect, RefObject } from 'react'
+import { select } from 'd3-selection'
+const Rectangle = () => {
+  const ref: RefObject<HTMLDivElement> = React.createRef()
+useEffect(() => {
+    draw()
+  })
+const draw = () => {
+    select(ref.current).append('p').text('Hello World')
+    select('svg').append('g').attr('transform', 'translate(250, 0)').append('rect').attr('width', 500).attr('height', 500).attr('fill', 'tomato')
+  }
+return (
+    <div className="Rectangle" ref={ref}>
+      <svg width="500" height="500">
+        <g transform="translate(0, 0)">
+          <rect width="500" height="500" fill="green" />
+        </g>
+      </svg>
+    </div>
+  )
+}
+export default Rectangle
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import Rectangle from '../components/Rectangle'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
@@ -9,7 +10,9 @@ const IndexPage = () => (
         <a>About</a>
       </Link>
     </p>
+  <Rectangle />
   </Layout>
+
 )
 
 export default IndexPage
